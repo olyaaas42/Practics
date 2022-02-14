b3176c6661fa7f79cf5703fe3bb0a6acf62cc9cd
olyaaas42 <olyaaas42@gmail.com>
2022-02-14 16:53:28 +0500

    Create class array & overload cout

--- Array/dynamicArray.cpp
+++ Array/dynamicArray.cpp
@@ -1,39 +1,49 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+class Array
+{
+    public:
+    int number;
+    int* dynamicArray = new int[number];
+
+    //перегрузка cout
+    friend ostream& operator<<(ostream &os, const Array &array) 
+    {
+        for (int i = 0; i < array.number; i++)
+            {
+                os << array.dynamicArray[i] << '\t';
+            }
+        cout << endl;
+        return os;
+    }
+};
+
 int main()
 {
-    int number = 5;
-    int* dynamicArray = new int[number];
-    dynamicArray[0] = 15;
-    dynamicArray[1] = 8;
-    dynamicArray[2] = 3;
-    dynamicArray[3] = 7;
-    dynamicArray[4] = 12;
-    
-    
-    cout << "Dymanic array: " << endl;
-    for (int i = 0; i < number; i++)
-    {
-        cout << dynamicArray[i] << '\t';
-    }
-    cout << endl;
+    Array array;
+    array.number = 5;
 
-    for(int i = 1; i < number; i++) //сортировка вставками
+    array.dynamicArray[0] = rand()%100;
+    array.dynamicArray[1] = rand()%100;
+    array.dynamicArray[2] = rand()%100;
+    array.dynamicArray[3] = rand()%100;
+    array.dynamicArray[4] = rand()%100;
+    
+    cout << "Dymanic array: " << array << endl;
+
+    //сортировка вставками
+    for(int i = 1; i < array.number; i++)
     {    
-        for(int j = i; j > 0 && dynamicArray[j-1] > dynamicArray[j]; j--)   // пока j>0 и элемент j-1 > j
+        for(int j = i; j > 0 && array.dynamicArray[j-1] > array.dynamicArray[j]; j--)   
         {
-            swap(dynamicArray[j-1], dynamicArray[j]);          // меняем местами элементы j и j-1                   
+            swap(array.dynamicArray[j-1], array.dynamicArray[j]);                       
         } 
     }
 
-    cout << "Sorted dymanic array: " << endl;
-    for (int i = 0; i < number; i++)
-    {
-        cout << dynamicArray[i] << '\t';
-    }
-    cout << endl;
+    cout << "Sorted dymanic array: " << array << endl;
 
-    delete [] dynamicArray;
+    delete [] array.dynamicArray;
 }
